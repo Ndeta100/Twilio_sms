7a39aa28346557c9b155995464e2fc13d0a5256d
Ndeta Innocent <you@example.com>
2022-01-23 15:57:46 +0200

    message fixes

--- config.js
+++ config.js
@@ -1,5 +1,8 @@
 module.exports={
     accountSid:'AC5ec1a4e7a95c873d5139b25e49d1ab8d',
     authToken:'23020b8f76e99d102b4cf7575f4c25b1',
-    PORT:3002
+    PORT:3002,
+    FROM_PHONE:'+16074994494',
+    TO_PHONE:'+37254200777'
+
 }
--- twilio.js
+++ twilio.js
@@ -1,12 +1,12 @@
 function twilio(){
-    const {accountSid,authToken, PORT} = require("./config");
+    const {accountSid,authToken, FROM_PHONE,TO_PHONE} = require("./config");
 const client = require('twilio')(accountSid, authToken);
 
 client.messages
       .create({
          body: 'Hi, dream come through I am a Twilio intern',
-         from: '+16074994494',
-         to: '+37254200777'
+         from: FROM_PHONE,
+         to: TO_PHONE
        })
       .then(message => console.log(message)).catch(err=>{
           console.log(err)
